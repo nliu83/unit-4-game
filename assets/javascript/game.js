157c7d4084e0cb7646f03e3755851b1a69d7c723
nliu83 <nliu8338@gmail.com>
2018-06-13 21:52:41 -0700

    Updates repo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 //generating random number between 19 to 120
 var targetNumber = Math.floor(Math.random() * 102) + 19;
 
@@ -15,30 +14,27 @@ $("#losses").text(losses);
 $("#currentscore").text(counter);
 //for each possible image, there is a value between 1 -  12
 
-var numberOptions = [
+var numberOptions = []
     
-    numb1 = Math.floor(Math.random() * 12) + 1,
-    numb2 = Math.floor(Math.random() * 12) + 1,
-    numb3 = Math.floor(Math.random() * 12) + 1,
-    numb4 = Math.floor(Math.random() * 12) + 1,
-]
+    for (i=0;i < 4 ; i++ ){
+
+        numberOptions[i] = Math.floor(Math.random() * 12) + 1;
+        
+        }
+
 console.log(numberOptions);
 
 function reset(){
     counter = 0;
     $("#currentscore").text(counter);
-    targetNumber = targetNumber = Math.floor(Math.random() * 102) + 19;
+    targetNumber = Math.floor(Math.random() * 102) + 19;
     console.log(targetNumber);
     $("#number-to-guess").text(targetNumber);
     
-    numberOptions = [
-        Math.floor(Math.random() * 12) + 1,
-        Math.floor(Math.random() * 12) + 1,
-        Math.floor(Math.random() * 12) + 1,
-        Math.floor(Math.random() * 12) + 1,
-    ]
+    for (i=0; i<numberOptions.length; i++) {
+        numberOptions[i] = Math.floor(Math.random() * 12) + 1;
+    }
     console.log(numberOptions);
-    
     } 
 
 
@@ -47,20 +43,22 @@ for (var i = 0; i < numberOptions.length; i++) {
 
     // For each iteration, we will create an imageCrystal
     var imageArray = $(".crystal-images");
-
+    console.log("1 : " + imageArray);
     // First each crystal will be given the class ".crystal-image".
     // This will allow the CSS to take effect.
     
     imageArray.addClass("crystal-image");
+    //console.log("2 : " + imageArray);
     
     // Each imageCrystal will be given a src link to the crystal image
-    imageArray.attr("crystal-image");
+    //imageArray.attr(".crystal-images");
 
+    //console.log("3 : " + imageArray);
     // Each imageCrystal will be given a data attribute called data-crystalValue.
     // This data attribute will be set equal to the array value.
 
     imageArray.attr("data-crystalvalue", numberOptions[i]);
-
+    
     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
     $(".crystal-images").append(imageArray);
   }
@@ -103,5 +101,3 @@ $(".crystal-image").on("click", function() {
       }
     
 });
-
-
